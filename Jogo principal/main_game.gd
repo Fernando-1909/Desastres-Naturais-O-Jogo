2f0a55226431281b1461ea41016d5a2d14a065c3
brunofoglake <bruoeduardonatal@gmail.com>
2026-05-18 11:58:41 -0300

    Commit 18/05
    implementado:
    - slot do dinheiro
    - popularidade
    - botão de pausa
    - *botão de mostrar mapa
    - ter como clicar na zona de prefeitura e praia *no mapa
    - ter uma parte mostrando o estagio das coisas (tantos turnos até chegar o desastre)

--- Jogo principal/main_game.gd
+++ Jogo principal/main_game.gd
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_mapa_pressed() -> void:
+	$MapOverlay.visible = true
