e516514163afb0fbd2d9d957318080e7f6900014
Fernando <fernando_macedo09@hotmail.com>
2026-06-01 17:05:34 -0300

    menu de pop-up
    botao de teste para abrir um menu de pop-up para as informações de construção

--- Jogo principal/main_game.gd
+++ Jogo principal/main_game.gd
@@ -10,7 +10,7 @@ func _ready() -> void:
 #func _process(delta: float) -> void:
 #	pass
 
-
+#abrir pop-up do mapa
 func _on_button_mapa_pressed() -> void:
 	$CanvasLayer/MapOverlay.visible = true
 
@@ -28,3 +28,18 @@ func _on_button_dinheiro_plus_pressed() -> void:
 func _on_button_dinheiro_minus_pressed() -> void:
 	Global.dinheiro -= 100
 	Global.popularidade -= 25
+
+#abrir pop-up de melhorias
+@onready var pop_up_scene = load("res://Jogo principal/building_hud.tscn")
+
+func _on_popup_teste_pressed() -> void:
+	print("popup apertado")
+	#print(pop_up_scene)
+	$CanvasLayer/BuildingHUD.visible = true
+	
+	#var new_pop_up = pop_up_scene.instantiate()
+	#add_child(new_pop_up)
+
+
+func _on_button_close_menu_pressed() -> void:
+	$CanvasLayer/BuildingHUD.visible = false
