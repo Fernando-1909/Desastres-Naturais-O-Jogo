17c60f844b0a6110e956aefb60e9ef8bbc1b8244
Fernando <fernando_macedo09@hotmail.com>
2026-06-09 13:53:18 -0300

    scripts consertados

--- Jogo principal/UI/hud.gd
+++ Jogo principal/UI/hud.gd
@@ -22,3 +22,13 @@ func _on_button_mapa_pressed() -> void:
 
 func _on_botao_teste_pressed() -> void:
 	FolderBlocker.liberarPraia()
+
+
+func _on_button_dinheiro_plus_pressed() -> void:
+	Global.dinheiro += 100
+	Global.popularidade += 50
+
+
+func _on_button_dinheiro_minus_pressed() -> void:
+	Global.dinheiro -= 100
+	Global.popularidade -= 25
