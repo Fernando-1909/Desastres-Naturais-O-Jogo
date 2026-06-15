1c57874e13d83522826e411ef22e3e1e45c5f874
Fernando <fernando_macedo09@hotmail.com>
2026-06-15 14:41:09 -0300

    clique funcional

--- Jogo principal/tile_layer_props.gd
+++ Jogo principal/tile_layer_props.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 func _unhandled_input(event: InputEvent) -> void:
 	# Verifica se é clique esquerdo do mouse
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-		print("✓ Clique detectado")  # Debug: ver se o clique está sendo registrado
+		#print("Clique detectado")  # Debug: ver se o clique está sendo registrado
 		
 		# Converte posição do mouse
 		var mouse_pos_local = to_local(event.global_position)
@@ -14,11 +14,10 @@ func _unhandled_input(event: InputEvent) -> void:
 		
 		#print("Posição do grid: ", grid_pos)  # Debug: mostra coordenadas
 		
-		# Verifica se existe tile na posição
-		var source_id = get_cell_source_id(grid_pos)
-		
-		#print("Source ID: ", source_id)  # Debug: mostra o ID da fonte
-		#print("Atlas coords: ", get_cell_atlas_coords(grid_pos))  # Debug
+		# Verifica se existe tile na posição DEBUG
+		# var source_id = get_cell_source_id(grid_pos)
+		# print("Source ID: ", source_id)  # Mostra o ID da fonte
+		# print("Atlas coords: ", get_cell_atlas_coords(grid_pos)) 
 		
 		# Tenta pegar os dados customizados
 		var tile_data: TileData = get_cell_tile_data(grid_pos)
