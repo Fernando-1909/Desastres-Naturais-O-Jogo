9beff8d82bec39316b1194269e23e452b2c7b065
brunofoglake <brunoeduardonatal@gmail.com>
2026-07-29 15:47:42 -0300

    Zoom e tema
    Implementação do zoom com o mouse e criação do tema global da interface

--- Jogo principal/Scripts/freecam.gd
+++ Jogo principal/Scripts/freecam.gd
@@ -24,3 +24,11 @@ func _input(event):
 		var new_zoom = zoom * event.factor
 		new_zoom = clamp(new_zoom, Vector2(zoom_min, zoom_min), Vector2(zoom_max, zoom_max))
 		zoom = new_zoom
+
+	if event is InputEventMouseButton and event.pressed:
+		if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+			var new_zoom = zoom * (1.0 + zoom_speed)
+			zoom = clamp(new_zoom, Vector2(zoom_min, zoom_min), Vector2(zoom_max, zoom_max))
+		elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+			var new_zoom = zoom * (1.0 - zoom_speed)
+			zoom = clamp(new_zoom, Vector2(zoom_min, zoom_min), Vector2(zoom_max, zoom_max))
